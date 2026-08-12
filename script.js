09379f05b8e80573be926b55f8b45c32286be6c8
Rodrigo Souza <rodrigoss01@hotmail.com>
2026-08-11 21:13:57 -0300

    Adiciona primeiro script de teste

---
+++ script.js
@@ -0,0 +1,9 @@
+// Meu primeiro código no Projeto 2
+console.log("Olá, mundo! O projeto 2 está funcionando.");
+
+// Um exemplo simples de soma
+const numero1 = 10;
+const numero2 = 5;
+const resultado = numero1 + numero2;
+
+console.log("O resultado da soma é: " + resultado); 
